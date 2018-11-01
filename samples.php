f3f18f11387319b4fb5731d9798febd16fdd73ea
EC2 Default User <ec2-user@ip-172-31-38-185.us-east-2.compute.internal>
2018-11-01 19:49:23 +0000

    Changed font style, logo, added include files

---
+++ samples.php
@@ -0,0 +1,29 @@
+        <?php
+            include("include/header.php");
+        ?>
+     	<!-- Begin Main Content -->
+        <h2>Audio Samples</h2>
+        <div class="row">
+            <audio controls class="col-md-4">
+                <source src="documents/sample.mp3" type="audio/mp3">
+            Your browser does not support the audio element.       
+            </audio>
+            <audio controls class="col-md-4">
+                <source src="documents/sample.mp3" type="audio/mp3">
+            Your browser does not support the audio element.       
+            </audio>
+            <audio controls class="col-md-4">
+                <source src="documents/sample.mp3" type="audio/mp3">
+            Your browser does not support the audio element.       
+            </audio>
+        </div>
+        <h2>Video Samples</h2>
+        <iframe width="420" height="315" src="https://www.youtube.com/embed/NW26pwMDUHA"></iframe>
+        <br />
+        <a href="https://www.youtube.com/watch?v=NW26pwMDUHA">Link to youtube video</a>
+        
+        
+        <!-- End Main Content -->
+    <?php
+            include("include/footer.php");
+    ?>
---
+++ studio.php
@@ -0,0 +1,101 @@
+        <?php
+            include("include/header.php");
+        ?>
+     	<!-- Begin Main Content -->
+        <h2>July 2017</h2>
+		<div class="table-responsive">
+        <table class="table table-bordered">
+        	<thead>
+            	<tr>
+                	<td>Sunday</td>
+                    <td>Monday</td>
+                    <td>Tuesday</td>
+                    <td>Wednesday</td>
+                    <td>Thursday</td>
+                    <td>Friday</td>
+                    <td>Saturday</td>
+                </tr>
+            </thead>
+            <tbody>
+            	<tr>
+                	<td>25</td>
+                    <td>26</td>
+                    <td>27</td>
+                    <td>28</td>
+                    <td>29</td>
+                    <td>30</td>
+                    <td>1</td>
+                </tr>
+            	<tr>
+                	<td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                    <td>6</td>
+                    <td>7</td>
+                    <td>8</td>
+                </tr>
+            	<tr>
+                	<td>9</td>
+                    <td>10</td>
+                    <td>11</td>
+                    <td>12</td>
+                    <td>13</td>
+                    <td>14<br /><button id="july14">Finish Website (click me)</button></td>
+                    <td>15</td>
+                </tr>
+            	<tr>
+                	<td>16</td>
+                    <td>17</td>
+                    <td>18</td>
+                    <td>19</td>
+                    <td>20</td>
+                    <td>21</td>
+                    <td>22</td>
+                </tr>
+            	<tr>
+                	<td>23</td>
+                    <td>24</td>
+                    <td>25</td>
+                    <td>26</td>
+                    <td>27</td>
+                    <td>28</td>
+                    <td>29</td>
+                </tr>
+            	<tr>
+                	<td>30</td>
+                    <td>31</td>
+                    <td>1</td>
+                    <td>2</td>
+                    <td>3</td>
+                    <td>4</td>
+                    <td>5</td>
+                </tr>
+            </tbody>
+        </table>
+        </div>
+        <div id="myModal" class="modal">
+        
+          	<!-- Modal content -->
+         	<div class="modal-content">
+           	 	<div class="modal-header">
+              		<span class="close">&times;</span>
+              		<h2>Event Header! (optional)</h2>
+            	</div>
+            	<div class="modal-body">
+              		<p>Brent Should be done with the initial design of the website!</p>
+              		<p>If he isn't, beat him over the head with a stick!</p>
+            	</div>
+            	<div class="modal-footer">
+              		<h3>Event Footer! (Also optional)</h3>
+            	</div>
+         	</div>
+		</div>
+        
+        <!-- End Main Content -->
+    </div>   
+   	<!-- End global div -->
+    <!-- Beging footer -->
+    <?php
+            include("include/footer.php");
+    ?>
