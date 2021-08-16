7571440257bba3915b03b43431b8db307993b4fd
Failbot7000 <brfree91@hotmail.com>
2021-08-16 00:03:59 +0000

    Form Test3

--- include/contactform.php
+++ include/contactform.php
@@ -7,7 +7,7 @@ if($_POST["message"]) {
 mail("brfree91@hotmail.com", "Website Contact Inquiry",
 
 
-$_POST["insert your message here"]. "From: an@email.address");
+$_POST["insert your message here"]. "From: brfree91@hotmail.com");
 
 
 }
