1f7f97579e28a289047e24342b66547849f75da2
Failbot7000 <brfree91@hotmail.com>
2022-06-25 23:11:14 +0000

    Updated Bio

--- events.php
+++ events.php
@@ -5,21 +5,20 @@
 
         <h2>Events</h2>
         
-        <h3>October-December 2021</h3>
-        <p>ABRSM Exams (Pending Dates)</p>
+        <h3>October 2022</h3>
+        <ul>
+            <li>Halloween recital (exact date TBD)</li>
+            
+        </ul>
+        <h3>November 12, 2022</h3>
+        <ul>
+            <li>22nd Annual Sonatina Festival in Verona, WI. <a href="http://www.musicbymickey.com/sonatinafestival/">Event link for more details</a>.</li>
+        </ul>
+        <h3></h3>
+        <ul>
+            <li>December 2022: Winter Holiday studio recital (exact date TBD)</li>
+        </ul>
 
-        <p>Saturday, November 13th: <a href="http://www.musicbymickey.com/sonatinafestival/">21st Annual Sonatina Festival</a>.</p>
-
-        <p>Saturday, December 4th, 11th, or 18th: 5th Annual Holiday Studio Recital. </p>
-
-        <p>Final Date Selection TBD. </p>
-        <h3>January 2022</h3>
-
-        <p>Saturday, January 22nd, 2022: Honors Festival Audition 8am-4pm (Grade 5-12 students only)</p>
-
-        <p>Sunday, January 30th, 2022: Honors Festival Performances at 1:30pm and 3:00pm. </p>
-
-        
         <!-- End Main Content -->
     </div>   
    	<!-- End global div -->
